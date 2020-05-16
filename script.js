995c7df2fd081304f9676a77289c6c7077db5836
Lavish jhamb <lavishjhamb1@gmail.com>
2020-05-16 21:03:04 +0530

    addded

---
+++ script.js
@@ -0,0 +1,49 @@
+// ******* TABS FUNCTION *************
+function openTabs(e, tabId) {
+    const tabContent = document.querySelectorAll('.content');
+    // hide all content
+    tabContent.forEach((content) => {
+        content.style.display = "none"
+    })
+    const tablinks = document.getElementsByClassName("tab-link");
+
+    for(const button of tablinks){
+        button.className = button.className.replace(" active-tabs", "");
+    }
+    e.currentTarget.className += " active-tabs";
+    // show content based on id passed
+    document.getElementById(tabId).style.display = "block";
+}
+
+
+// ACCORDION
+// Get all Accordion and Panel
+let accHeading = document.querySelectorAll(".accordion");
+let accPanel = document.querySelectorAll(".accordion-panel");
+// document.addEventListener("domContentLoaded", showPanel)
+
+for (let i = 0; i < accHeading.length; i++) {
+    // Execute whenever an accordion is clicked 
+    accHeading[i].onclick = function () {
+        // if (this.nextElementSibling.style.maxHeight) {
+        //     // hidePanels();     // Hide All open Panels 
+        // } else {
+            showPanel(this);  // Show the panel
+        // }
+    };
+}
+
+// Function to Show a Panel
+function showPanel(elem) {
+    hidePanels();
+    elem.classList.add("active-accordion");
+    elem.nextElementSibling.style.maxHeight = "300px"
+}
+
+// Function to Hide all shown Panels
+function hidePanels() {
+    for (let i = 0; i < accPanel.length; i++) {
+        accPanel[i].style.maxHeight = null;
+        accHeading[i].classList.remove("active-accordion");
+    }
+}
